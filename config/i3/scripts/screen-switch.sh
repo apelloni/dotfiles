2d3d6c4dd411df09fa38ece679220b8691224650
apelloni <apelloni90@gmail.com>
2022-12-20 10:12:44 +0100

    wm configuration

---
+++ config/i3/scripts/screen-switch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# First argument: Primary monitor
+# Second argument: Secondary monitor
+if  xrandr --listactivemonitors | grep -q "$2"; then
+	xrandr --output "$1" --auto
+	xrandr --output "$2" --off
+    ~/.config/i3/scripts/resize_screen 1
+elif  xrandr --listactivemonitors | grep -q "$1"; then
+	xrandr --output "$2" --auto
+	xrandr --output "$1" --off
+    ~/.config/i3/scripts/resize_screen 1
+else
+	xrandr --output "$1" --auto
+    ~/.config/i3/scripts/resize_screen 1
+fi
