61f9997928d51fffa78fe9af9f21af0cdd0d6c8c
apelloni <apelloni90@gmail.com>
2023-10-02 10:54:25 +0200

    fixed monitor switch to accomodate ubs-c output DP-1 and DP-2

--- config/i3/scripts/screen-switch.sh
+++ config/i3/scripts/screen-switch.sh
@@ -2,14 +2,18 @@
 
 # First argument: Primary monitor
 # Second argument: Secondary monitor
-if  xrandr --listactivemonitors | grep -q "$2"; then
+EXTERNAL_SCREEN=$(xrandr | grep " connected" | grep -Eo "^$2")
+
+if  xrandr --listactivemonitors | grep -Eq " $2"; then
 	xrandr --output "$1" --auto
-	xrandr --output "$2" --off
+	xrandr --output "$EXTERNAL_SCREEN" --off
     ~/.config/i3/scripts/resize_screen 1
 elif  xrandr --listactivemonitors | grep -q "$1"; then
-	xrandr --output "$2" --auto
+	xrandr --output "$EXTERNAL_SCREEN" --auto
 	xrandr --output "$1" --off
-    ~/.config/i3/scripts/resize_screen 1.3
+    ~/.config/i3/scripts/resize_screen 1
+#    sleep 1
+#    ~/.config/i3/scripts/resize_screen 1.3
 else
 	xrandr --output "$1" --auto
     ~/.config/i3/scripts/resize_screen 1
