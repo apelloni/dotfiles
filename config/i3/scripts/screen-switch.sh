433f92f64572925a59c2135c42dc518ed5b9c286
apelloni <apelloni90@gmail.com>
2023-10-03 09:45:43 +0200

    turn off screen before casting to second one to avoid auto sizing problems

--- config/i3/scripts/screen-switch.sh
+++ config/i3/scripts/screen-switch.sh
@@ -5,12 +5,12 @@
 EXTERNAL_SCREEN=$(xrandr | grep " connected" | grep -Eo "^$2")
 
 if  xrandr --listactivemonitors | grep -Eq " $2"; then
-	xrandr --output "$1" --auto
 	xrandr --output "$EXTERNAL_SCREEN" --off
+	xrandr --output "$1" --auto
     ~/.config/i3/scripts/resize_screen 1
 elif  xrandr --listactivemonitors | grep -q "$1"; then
-	xrandr --output "$EXTERNAL_SCREEN" --auto
 	xrandr --output "$1" --off
+	xrandr --output "$EXTERNAL_SCREEN" --auto
     ~/.config/i3/scripts/resize_screen 1
 #    sleep 1
 #    ~/.config/i3/scripts/resize_screen 1.3
